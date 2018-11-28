b49e46567ede17605b9643bf297db59cdea2ffe3
Borys Koblents <borys.koblents@keenethics.com>
2018-11-28 21:19:48 +0200

    zip

--- src/api/best-change/controller.js
+++ src/api/best-change/controller.js
@@ -2,37 +2,82 @@ const http = require('http')
 const fs = require('fs')
 const StreamZip = require('node-stream-zip')
 
-
-
 module.exports = {
-  index: ({ querymen: { query, select, cursor } }, res, next) => {
+  index: ({
+    querymen: {
+      query,
+      select,
+      cursor
+    }
+  }, res, next) => {
     http.get('http://api.bestchange.ru/info.zip', (data) => {
-      const { statusCode } = data
+      const {
+        statusCode
+      } = data
       if (statusCode !== 200) {
         res.status(400).send('smth went wrong')
         throw console.error(data)
       } else {
-        fs.writeFileSync('info.zip', data)
-        const file = fs.readFileSync('info.zip')
-        const zip = new StreamZip({
-          file: 'info.zip',
-          storeEntries: true
+        const file = fs.createWriteStream('info.zip')
+        data.pipe(file)
+        file.on('finish', () => {
+          file.close(zipFunc) // close() is async, call cb after close completes.
         })
-        console.log('Entries read: ' + zip.entriesCount)
-        for (const entry of Object.values(zip.entries())) {
-          const desc = entry.isDirectory ? 'directory' : `${entry.size} bytes`
-          console.log(`Entry ${entry.name}: ${desc}`)
+
+        const zipFunc = () => {
+          const zip = new StreamZip({
+            file: 'info.zip',
+            storeEntries: true
+          })
+          for (const entry of Object.values(zip.entries())) {
+            const fileArr = []
+            zip.stream(entry, (err, stm) => {
+              if (err) {
+                console.log(err, '-----erro')
+              } else {
+                console.log(stm)
+                fileArr.push(stm)
+              }
+            })
+
+            console.log('The content of info.zip is: ' + fileArr)
+          }
+          
+          zip.on('ready', () => {
+            // Take a look at the files
+            // console.log('Entries read: ' + zip.entriesCount)
+            // for (const entry of Object.values(zip.entries())) {
+            //   const desc = entry.isDirectory ? 'directory' : `${entry.size} bytes`
+            //   console.log(`Entry ${entry.name}: ${desc}`)
+            //   console.log('entry full------', entry)
+            //   // Read a file in memory
+            //   let zipDotDatContents = zip.entryDataSync(entry)
+            //   console.log(zipDotDatContents)
+            //   fileArr.push(zipDotDatContents)
+            // }
+            res.status(200).json('success')
+          })
+          zip.on('entry', entry => {
+            // you can already stream this entry,
+            // without waiting until all entry descriptions are read (suitable for very large archives)
+            console.log(entry)
+            console.log(`Read entry ${entry.name}`)
+          })
+          setTimeout(() => {
+            console.log('test')
+            zip.close()
+          }, 5000)
         }
-        // console.log(file)
-        res.status(200).json('success')
-        zip.close()
+
+        // zip.close()
+
         fs.close(0, (err) => {
           if (err) throw console.error(err)
         })
 
-        zip.on('error', err => {
-          console.error(err)
-        })
+        // zip.on('error', err => {
+        //   console.error(err)
+        // })
       }
     })
     // BestChange.find(query, select, cursor)
