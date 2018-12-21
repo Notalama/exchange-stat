2efb7a76924189aaf4680a580264dbde16ea417a
Serhiy Zalavskyi <warez45@gmail.com>
2018-12-21 11:51:04 +0200

    merge

--- exchange-stat-front/src/main.js
+++ exchange-stat-front/src/main.js
@@ -8,6 +8,7 @@ import VueGoodTablePlugin from 'vue-good-table';
 import 'vue-good-table/dist/vue-good-table.css'
 import '../node_modules/materialize-css/dist/css/materialize.min.css';
 import '../node_modules/materialize-css/dist/js/materialize.min.js';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
 Vue.use(VueGoodTablePlugin);
 new Vue({
   render: h => h(App),
