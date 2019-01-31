3d7c570aa7928fe55e9e74dc23a9bbb27458dec0
Borys Koblents <borys.koblents@keenethics.com>
2019-01-31 11:21:03 +0200

    calcBonus fix

--- src/services/helpers/format-and-filter.js
+++ src/services/helpers/format-and-filter.js
@@ -185,7 +185,7 @@ function calcCommission (rate, commissions) {
 }
 
 function calcBonus (rate, bonus) {
-  const forAll = bonus.from && !bonus.to
+  const forAll = !bonus.from && !bonus.to
   const forOneCurr = !bonus.from && bonus.to && rate[1] === bonus.to
   const forPair = bonus.from && bonus.to && rate[1] === bonus.to && bonus.from === rate[0]
   if (forAll || forOneCurr || forPair) +rate[4] > 1 ? rate[4] = +rate[4] * (+bonus.multi / 100 + 1) : rate[3] = +rate[3] / (+bonus.multi / 100 + 1)
