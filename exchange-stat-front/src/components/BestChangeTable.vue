a4a144b2aa670e09c1470a3a63827623196797ea
Borys Koblents <borys.koblents@keenethics.com>
2019-02-06 15:56:40 +0200

    reload

--- exchange-stat-front/src/components/BestChangeTable.vue
+++ exchange-stat-front/src/components/BestChangeTable.vue
@@ -204,7 +204,7 @@ export default {
         this.interval += interval;
     },
     reloadInterval: function() {
-      // this.loadItems()
+      this.loadItems()
       setTimeout(() => {
         this.reloadInterval();
       }, this.interval);
