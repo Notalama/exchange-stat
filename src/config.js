ef670b512f065d23618e309464f80df69817564c
Borys Koblents <borys.koblents@keenethics.com>
2019-03-12 13:17:51 +0200

    masterkeyRemove

--- src/config.js
+++ src/config.js
@@ -26,7 +26,7 @@ const config = {
     port: process.env.PORT || 9000,
     ip: process.env.IP || '0.0.0.0',
     apiRoot: process.env.API_ROOT || '',
-    masterKey: requireProcessEnv('MASTER_KEY'),
+    // masterKey: requireProcessEnv('MASTER_KEY'),
     mongo: {
       options: {
         db: {
